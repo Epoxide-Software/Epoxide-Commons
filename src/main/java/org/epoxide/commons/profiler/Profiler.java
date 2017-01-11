e8655e59c4109b56d7e68943c3153c1ca8738bb4
Tyler Hancock <darklime@live.ca>
2017-01-11 01:31:33 -0700

    Added a way to track a specific entry.

--- src/main/java/org/epoxide/commons/profiler/Profiler.java
+++ src/main/java/org/epoxide/commons/profiler/Profiler.java
@@ -1,10 +1,13 @@
 package org.epoxide.commons.profiler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Profiler {
 
 	private final Logger log;
+	private final List<ProfileEntry> entries = new ArrayList<>();
 	
 	private boolean enabled;
 	
@@ -18,13 +21,26 @@ public class Profiler {
 		this.log = log;
 	}
 	
-	public void setEnabled(boolean enabled) {
+	public Profiler setEnabled(boolean enabled) {
 		
 		this.enabled = enabled;
+		return this;
 	}
 	
 	public boolean isEnabled() {
 		
 		return this.enabled;
+	}
+	
+	public static class ProfileEntry {
+		
+		private final String name;
+		private Long time;
+		private boolean ended;
+		
+		public ProfileEntry (String name) {
+			
+			this.name = name;
+		}
 	}
 }
