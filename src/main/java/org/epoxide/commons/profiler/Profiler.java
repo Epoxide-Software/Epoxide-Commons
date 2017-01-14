5ec45a230bec55e7799571d2aa90227818fded53
Tyler Hancock <darklime@live.ca>
2017-01-14 02:16:05 -0700

    Polished up the code, and added documentation.

--- src/main/java/org/epoxide/commons/profiler/Profiler.java
+++ src/main/java/org/epoxide/commons/profiler/Profiler.java
@@ -1,81 +1,152 @@
 package org.epoxide.commons.profiler;
 
+import org.epoxide.commons.EpoxideCommons;
+
 /**
- * A simple profiler for timing your code. The provided system will allow you to time a
- * process, along with sub processes.
+ * A simple tool for profiling code. The system revolves around {@link ProfilerEntry} which is
+ * an object which represents a given task being profiled. A new entry is created every time
+ * {@link #start(String)} is called. Sub entries are supported, so you can create a new entry
+ * while another one is being profiled, and it will be added as a sub entry. Once the task
+ * being profiled is done remember to call {@link #stop()} to stop the current process. You
+ * should call the stop method once for every time the start method was called, to ensure all
+ * sub entries have been marked as complete.
+ *
+ * Please note that this system also includes the ability to enable/disable the profiler
+ * arbitrarily. This is to allow profiling code to be included in production code with a
+ * negligible performance impact. While profiling production code is not advised, it may be
+ * useful for applications with a debug mode.
+ *
+ * @author Tyler Hancock (Darkhax)
  */
 public class Profiler {
-
-    private final ProfileEntry main;
-
+    
+    /**
+     * The main entry for the profiler. All other entries by the profiler will be sub entities
+     * of this.
+     */
+    private final ProfilerEntry main;
+    
+    /**
+     * The state of the profiler. When disabled (false) no new entries will be created. Allows
+     * for profiler code to be slipped into production code with minimal performance impact.
+     */
     private boolean enabled;
-    private ProfileEntry current;
-
+    
+    /**
+     * The entry that is currently in progress.
+     */
+    private ProfilerEntry current;
+    
+    /**
+     * Constructor for a profiler. This should be used when you want to create your profiler.
+     *
+     * @param profileName The name for the profiler. This will be included in the {@link #main}
+     *        entry.
+     */
     public Profiler (String profileName) {
-
-        this.main = new ProfileEntry("main", true);
+        
+        this.main = new ProfilerEntry("main");
         this.main.setTime(System.nanoTime());
         this.current = this.main;
     }
-
+    
+    /**
+     * Starts profiling a new entry. Make sure that you call {@link #stop()} when the entry is
+     * done.
+     *
+     * @param processName The name of the thing you are profiling.
+     */
     public void start (String processName) {
-
+        
         if (this.isEnabled()) {
-
-            final ProfileEntry entry = new ProfileEntry(processName);
-
+            
+            final ProfilerEntry entry = new ProfilerEntry(processName);
+            
             if (this.current != null)
                 this.current.addSubEntry(entry);
-
+            
             this.current = entry;
             this.current.setTime(System.nanoTime());
         }
     }
-
-    public ProfileEntry stop () {
-
+    
+    /**
+     * Stops the current profile entry. This will mark it as complete and update the time to
+     * reflect the duration. This will also switch {@link #current} with the parent of the
+     * current process.
+     *
+     * @return The ProfilerEntry that was stopped.
+     */
+    public ProfilerEntry stop () {
+        
         if (this.isEnabled()) {
-
+            
             if (this.current == null)
-                return null;
-
+                EpoxideCommons.getLogger().warning("An attempt was made to stop profiling a process, but there are no current processes being profiled for " + this.main.getName());
+            
             this.current.setTime(System.nanoTime() - this.current.getTime());
             this.current.setComplete(true);
-
-            final ProfileEntry ending = this.current;
-
+            
+            final ProfilerEntry ending = this.current;
+            
             if (this.current.hasParent())
                 this.current = this.current.getParent();
-
+            
             return ending;
         }
-
+        
         return null;
     }
-
-    public void addNote (String note) {
-
+    
+    /**
+     * Adds a note to the current profiler entry.
+     *
+     * @param note The note to add.
+     */
+    public void note (String note) {
+        
         this.current.addNote(note);
     }
-
+    
+    /**
+     * Updates the enabled status of the profiler.
+     *
+     * @param enabled Whether the profiler should be enabled.
+     * @return The same profiler instance, provided for quality of life.
+     */
     public Profiler setEnabled (boolean enabled) {
-
+        
         this.enabled = enabled;
         return this;
     }
-
+    
+    /**
+     * Checks if the profiler is enabled.
+     *
+     * @return Whether or not the profiler was enabled.
+     */
     public boolean isEnabled () {
-
+        
         return this.enabled;
     }
-
-    public ProfileEntry getCurrentEntry () {
-
+    
+    /**
+     * Gets the current entry being profiled.
+     *
+     * @return The current entry being profiled.
+     */
+    public ProfilerEntry getCurrentEntry () {
+        
         return this.current;
     }
-
-    public ProfileEntry getMain () {
-
+    
+    /**
+     * Gets the main profiler entry. This should be the first entry created when constructed.
+     *
+     * @return The main profiler entry.
+     */
+    public ProfilerEntry getMain () {
+        
         return this.main;
     }
 }
---
+++ src/main/java/org/epoxide/commons/profiler/ProfilerEntry.java
@@ -0,0 +1,189 @@
+package org.epoxide.commons.profiler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Used to represent a task/process in the profiler. All of the base data is held within an
+ * object of this type.
+ *
+ * @author Tyler Hancock (Darkhax)
+ */
+public class ProfilerEntry {
+    
+    /**
+     * The name of the entry.
+     */
+    private String name;
+    
+    /**
+     * The time of the entry. While {@link #isComplete()} returns false, this will be the start
+     * time. When completed this instead represents duration time.
+     */
+    private long time;
+    
+    /**
+     * The completion flag for the entry.
+     */
+    private boolean completed;
+    
+    /**
+     * The parent profiler entry. This will be null for the main entry.
+     */
+    private ProfilerEntry parent;
+    
+    /**
+     * The list of all sub entries.
+     */
+    private final List<ProfilerEntry> subEntries = new ArrayList<>();
+    
+    /**
+     * The list of notes which have been added to this entry.
+     */
+    private final List<String> notes = new ArrayList<>();
+    
+    /**
+     * Constructor for a profiler entry. Only handles the name. The rest of the logic is done
+     * in {@link Profiler}.
+     *
+     * @param name The name of the entry.
+     */
+    public ProfilerEntry (String name) {
+        
+        this.name = name;
+    }
+    
+    /**
+     * Gets the name of the profiler entry.
+     *
+     * @return The name of the profiler entry.
+     */
+    public String getName () {
+        
+        return this.name;
+    }
+    
+    /**
+     * Gets the time for the profiler entry. The meaning of this value changes based on the
+     * result of {@link #isComplete()}. When the entry is not complete, this will be the start
+     * time, and when it is true it will be the total duration time.
+     *
+     * @return The time value for the entry.
+     */
+    public long getTime () {
+        
+        return this.time;
+    }
+    
+    /**
+     * Sets the time value to an arbitrary value.
+     *
+     * @param time The long to use for the time value.
+     */
+    public void setTime (long time) {
+        
+        this.time = time;
+    }
+    
+    /**
+     * Checks if the entry has been completed. This will bet set to true after
+     * {@link Profiler#stop()} has been called while this entry is the current entry.
+     *
+     * @return Whether or not the entry has been completed.
+     */
+    public boolean isComplete () {
+        
+        return this.completed;
+    }
+    
+    /**
+     * Sets the completed value to an arbitrary state.
+     *
+     * @param complete The new boolean to use for the completed state.
+     */
+    public void setComplete (boolean complete) {
+        
+        this.completed = complete;
+    }
+    
+    /**
+     * Adds a sub entry to this entry. This will also make this entry the parent of the the sub
+     * entry.
+     *
+     * @param entry The sub entry to add.
+     */
+    public void addSubEntry (ProfilerEntry entry) {
+        
+        this.subEntries.add(entry);
+        entry.setParent(this);
+    }
+    
+    /**
+     * Gets a list of all sub entries. This will never be null, but it can be empty.
+     *
+     * @return The list of all sub entries.
+     */
+    public List<ProfilerEntry> getSubEntries () {
+        
+        return this.subEntries;
+    }
+    
+    /**
+     * Gets the parent for the entry. If this is null, the entry is likely the main entry for
+     * the profiler.
+     *
+     * @return The parent entry.
+     */
+    public ProfilerEntry getParent () {
+        
+        return this.parent;
+    }
+    
+    /**
+     * Sets the parent entry for the sub entry. This will also prefix the name of the entry
+     * with the name of the parent entry.
+     *
+     * @param parent The entry to set as the parent.
+     */
+    public void setParent (ProfilerEntry parent) {
+        
+        this.parent = parent;
+        this.name = parent.getName() + "#" + this.getName();
+    }
+    
+    /**
+     * Checks if this entry has a parent.
+     *
+     * @return Whether or not the entry has a parent.
+     */
+    public boolean hasParent () {
+        
+        return this.getParent() != null;
+    }
+    
+    /**
+     * Adds a note to the entry.
+     *
+     * @param note The note to add.
+     */
+    public void addNote (String note) {
+        
+        this.notes.add(note);
+    }
+    
+    /**
+     * Gets a list of notes for the entry. This will never be null, but it can be empty.
+     *
+     * @return A list of notes for the entry.
+     */
+    public List<String> getNotes () {
+        
+        return this.notes;
+    }
+    
+    @Override
+    public String toString () {
+        
+        return this.getName() + " - " + this.getTime() + "NS " + (this.isComplete() ? "Completed" : "Incomplete");
+    }
+}
